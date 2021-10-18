3ced4899e2489eb7c3ad76b8e1fe1d9ed8aadb18
Héctor <hfaildeestevez@danielcastelao.org>
2021-10-18 10:34:17 +0200

    Initial commit y diseño grafico

---
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.simondice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
