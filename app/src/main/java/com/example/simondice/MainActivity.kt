f772ec7ac11fb9f42e3614381d9b769a9936df81
Héctor <hfaildeestevez@danielcastelao.org>
2021-10-21 14:04:58 +0200

    Contador de ronda instanciado, log cat declarado

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,22 +12,42 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val empezarPartida: Button = findViewById(R.id.bJugar)
-
+//Listener para botón play.
+        //Llamo al Log para que me de informacion acerca de lo que esta pasando
+        //Llamo a los metodos.
         empezarPartida.setOnClickListener{
-            Log.INFO
-
-            mostrarRonda()
+            Log.i("estado" , "Boton play presionado")
+            var contador=0
+            mostrarRonda(contador)
             ejecutarSecuencia()
         }
 
     }
-    private fun mostrarRonda() {
+    //Método mostrar ronda
+    private fun mostrarRonda(contador:Int){
+    Log.i("ronda", "Ronda mostrada")
+
+        val t:TextView=findViewById(R.id.contadorRonda)
+        t.setText("Ronda: "+contador.toString())
+
+
+
     }
+    //Metodo para que me de una secuencia a seguir
     private fun ejecutarSecuencia() {
+    Log.i("secuencia","Secuencia Ejecutada")
+
     }
+
     private fun mensajeUsuario() {
+    Log.i("mensaje","Mensaje mostrado")
+
     }
+
     private fun comprobarSecuencia() {
+        Log.i("Secuencia","Secuencia comprobada")
+
+
     }
 
 }
