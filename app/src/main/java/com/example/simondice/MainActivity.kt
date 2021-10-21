5d66763d56411ffee571c08e8864f8714a1bfc56
Héctor <hfaildeestevez@danielcastelao.org>
2021-10-21 14:30:08 +0200

    reformateo de codigo

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -12,40 +12,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val empezarPartida: Button = findViewById(R.id.bJugar)
-//Listener para botón play.
-        //Llamo al Log para que me de informacion acerca de lo que esta pasando
+    //Listener para botón play.
+        // Llamo al Log para que me de informacion acerca de lo que esta pasando
         //Llamo a los metodos.
-        empezarPartida.setOnClickListener{
-            Log.i("estado" , "Boton play presionado")
-            var contador=0
-            mostrarRonda(contador)
-            ejecutarSecuencia()
+            empezarPartida.setOnClickListener{
+                Log.i("estado" , "Boton play presionado")
+                var contador=0
+                    mostrarRonda(contador)
+                    ejecutarSecuencia()
         }
 
     }
     //Método mostrar ronda
-    private fun mostrarRonda(contador:Int){
-    Log.i("ronda", "Ronda mostrada")
+        private fun mostrarRonda(contador:Int){
+            Log.i("ronda", "Ronda mostrada")
 
         val t:TextView=findViewById(R.id.contadorRonda)
-        t.setText("Ronda: "+contador.toString())
-
-
+            t.setText("Ronda: "+contador.toString())
 
     }
     //Metodo para que me de una secuencia a seguir
-    private fun ejecutarSecuencia() {
-    Log.i("secuencia","Secuencia Ejecutada")
+        private fun ejecutarSecuencia() {
+            Log.i("Secuencia","Secuencia Ejecutada")
 
     }
 
-    private fun mensajeUsuario() {
-    Log.i("mensaje","Mensaje mostrado")
+        private fun mensajeUsuario() {
+            Log.i("mensaje","Mensaje mostrado")
 
     }
 
-    private fun comprobarSecuencia() {
-        Log.i("Secuencia","Secuencia comprobada")
+        private fun comprobarSecuencia() {
+            Log.i("Secuencia","Secuencia comprobada")
 
 
     }
