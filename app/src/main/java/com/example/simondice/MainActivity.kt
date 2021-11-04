b7c3b598969ef01c42d30e09453de2c52fd81ec7
Héctor <hfaildeestevez@danielcastelao.org>
2021-11-04 13:38:04 +0100

    modificaciones menores

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.simondice
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -12,48 +13,55 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    var contador=0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val empezarPartida: Button = findViewById(R.id.bJugar)
-    //Listener para botón play.
+        //Listener para botón play.
         // Llamo al Log para que me de informacion acerca de lo que esta pasando
         //Llamo a los metodos.
-            empezarPartida.setOnClickListener{
+        empezarPartida.setOnClickListener{
 
-                //Creo el texto que quiero que se muestre en la toast y el tamaño.
-                val text = "GAME ON!"
-                val duration = Toast.LENGTH_SHORT
-                //llamo a la toast para que se muestre.
-                val toast = Toast.makeText(applicationContext, text, duration)
-                toast.show()
+            //Creo el texto que quiero que se muestre en la toast y el tamaño.
+            val text = "GAME ON!"
+            val duration = Toast.LENGTH_SHORT
+            //llamo a la toast para que se muestre.
+            val toast = Toast.makeText(applicationContext, text, duration)
+            toast.show()
 
-                Log.i("estado" , "Boton play presionado")
+            Log.i("estado" , "Boton play presionado")
 
-                //Creo contador para que vaya acumulando las rondas
-                var contador=0
-                    mostrarRonda(contador)
+            //Creo contador para que vaya acumulando las rondas
 
+            mostrarRonda()
 
-                val job= GlobalScope.launch (Dispatchers.Main){
-                    ejecutarSecuencia()
-                }
-
+            val job= GlobalScope.launch (Dispatchers.Main){
+                secuenciaBotones()
             }
+
+        }
 
     }
     //Método mostrar ronda
-        private fun mostrarRonda(contador:Int){
-            Log.i("ronda", "Ronda mostrada")
+    private fun mostrarRonda(){
+        Log.i("ronda", "Ronda mostrada")
         //Añado el contenido del contador a un textView
         val t:TextView=findViewById(R.id.contadorRonda)
-            t.setText("Ronda: "+contador.toString())
+        contador++
 
+        t.setText("Ronda: "+contador.toString())
     }
     //Metodo para que me de una secuencia a seguir
-       suspend fun ejecutarSecuencia() {
-            Log.i("Secuencia","Secuencia Ejecutada")
+    suspend fun ejecutarSecuencia() {
+        Log.i("Secuencia","Secuencia Ejecutada")
+
+
+
 
         delay(3000L)
         Log.i("estado", "Con delay")
@@ -62,15 +70,42 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-        private fun mensajeUsuario() {
-            Log.i("mensaje","Mensaje mostrado")
+    private fun mensajeUsuario() {
+        Log.i("mensaje","Mensaje mostrado")
 
     }
 
-        private fun comprobarSecuencia() {
-            Log.i("Secuencia","Secuencia comprobada")
+    private fun comprobarSecuencia() {
+        Log.i("Secuencia","Secuencia comprobada")
 
 
     }
+    suspend fun secuenciaBotones() {
+
+        // val secuencia = ronda +1
+        val Colores = arrayOf("#008000","#FFFF00","#3498DB","#EC4849")
+        var arrayBotones = hashMapOf<Int,Button>()
+        arrayBotones[0]= findViewById(R.id.bVerde)
+        arrayBotones[1]= findViewById(R.id.bAmarillo)
+        arrayBotones[2] = findViewById(R.id.bAzul)
+        arrayBotones[3] = findViewById(R.id.bRojo)
+
+        var secuencia:Array<Int> = arrayOf()
+        var random = (0..3).random()
+        secuencia = arrayOf(random)
+        for(i in 1..contador) {
+            delay(500L)
+            arrayBotones[contador]?.setBackgroundColor(Color.WHITE)
+            delay(500L)
+            arrayBotones[contador]?.setBackgroundColor(Color.parseColor(Colores[contador]))
+            //var todosColores = arrayListOf(cuatroColores[random])
+            //val b: Button = arrayBotones[random]!!
+            //delay(1000L)
+            //arrayBotones[ramdon]?.setBackgroundColor()
+            //b.foregroundTintList = ColorStateList.valueOf(Color.WHITE)
+        }
+        //return secuencia
+    }
+
 
 }
