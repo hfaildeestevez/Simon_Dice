3253487b5642f6a82ae336d8f999cd93045232e6
Héctor <hfaildeestevez@danielcastelao.org>
2021-11-11 14:31:20 +0100

    Codigo comentado, cambios menores hechos

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -15,8 +15,12 @@ import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
+
+    //contador para mostrar las rondas
     private var contador:Int=0
+    //array para guardar la secuencia
     var secuencia= arrayOf<Int>()
+    //array para guardar la comprobacion de la secuencia
     var comprobarSecuencia=arrayOf<Int>()
     private var contadorSecuencia:Int=0
     var compo=true
@@ -24,9 +28,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val empezarPartida: Button = findViewById(R.id.bJugar)
+        //listener del boton jugar
         empezarPartida.setOnClickListener {
-            //Aqui iria lo que ejecutaria darle empezar a partida
             Log.i("Estado","Boton Jugar pulsado")
+            //aqui llamo al metodo mostrarRonda
             mostrarRonda()
 
             //creo la variable job para despues poder pausar la corrutina o pararla
@@ -34,11 +39,15 @@ class MainActivity : AppCompatActivity() {
                 //llamo al metodo ejecutar Secuencia para que me ejecute el juego
                 empezarPartida.visibility=Button.INVISIBLE
                 ejecutarSecuencia()
+                //toast definida en mensajeUsuario
                 mensajeUsuario(2)
 
             }
         }
+        //listener de boton comprobar y de los colores
         val bcomprobar:Button=findViewById(R.id.bComprobar)
+        //si los botones pulsados por el jugador son inferiores a los de la secuencia avisa de que
+        // deberias volver a primaria para aprender a contar
         bcomprobar.setOnClickListener{
             if(secuencia.size==comprobarSecuencia.size){
                 comprobarSecuencia()
@@ -82,45 +91,44 @@ class MainActivity : AppCompatActivity() {
     }
     suspend fun ejecutarSecuencia(){
         mensajeUsuario(1)
-        /*con lo del log voy comprobando */
         Log.i("Estado","Se ejecuta el juego")
-        //hace secuencia
+        //Se ejecuta la secuencia
+        //El color de los botones se cambia a blanco durante medio segundo para simular un parpadeo
         for (b in secuencia){
             when{
                 b==1->{val bVerde: Button = findViewById(R.id.bVerde)
                     bVerde.setBackgroundColor(Color.parseColor("#FFFFFF"))
-                    delay(1000L)
+                    delay(500L)
                     bVerde.setBackgroundColor(Color.parseColor("#179205"))
-                    delay(1000L)
+                    delay(500L)
                     Log.i("Estado","Verde")
                 }
                 b==2->{val bRojo: Button = findViewById(R.id.bRojo)
                     bRojo.setBackgroundColor(Color.parseColor("#FFFFFF"))
-                    delay(1000L)
+                    delay(500L)
                     bRojo.setBackgroundColor(Color.parseColor("#C50505"))
-                    delay(1000L)
+                    delay(500L)
                     Log.i("Estado","Rojo")
                 }
                 b==3->{val bAmarillo: Button = findViewById(R.id.bAmarillo)
                     bAmarillo.setBackgroundColor(Color.parseColor("#FFFFFF"))
-                    delay(1000L)
+                    delay(500L)
                     bAmarillo.setBackgroundColor(Color.parseColor("#FFFE00"))
-                    delay(1000L)
+                    delay(500L)
                     Log.i("Estado","Amarillo")
                 }
                 b==4->{val bAzul: Button = findViewById(R.id.bAzul)
                     bAzul.setBackgroundColor(Color.parseColor("#FFFFFF"))
-                    delay(1000L)
+                    delay(500L)
                     bAzul.setBackgroundColor(Color.parseColor("#03FEED"))
-                    delay(1000L)
+                    delay(500L)
                     Log.i("Estado","Azul")
                 }
             }
 
         }
-
-        //secuencia usuario
     }
+    //Aqui declaro las toast con los mensajes
     private fun mensajeUsuario(key:Int){
         Log.i("Estado","Mensaje por toast al usuario")
         val mensaje =when{
@@ -132,6 +140,7 @@ class MainActivity : AppCompatActivity() {
         }
         Toast.makeText(this.applicationContext, mensaje, Toast.LENGTH_SHORT).show()
     }
+    //Metodo para que compruebe la secuencia y la aumente en su contador
     private fun comprobarSecuencia() {
         Log.i("Estado","Comprobar la secuencia del jugador")
         for(s in secuencia){
@@ -157,7 +166,7 @@ class MainActivity : AppCompatActivity() {
             finalizarPartida()
         }
     }
-
+        //metodo para finalizar la partida en el momento que el usuario fallo
     private fun finalizarPartida(){
         Log.i("Estado","Fin Partida")
         secuencia= arrayOf()
@@ -169,6 +178,7 @@ class MainActivity : AppCompatActivity() {
         empezarPartida.visibility=Button.VISIBLE
         comprobarSecuencia=arrayOf()
     }
+    //corrutina que estoy reutilizando para que cada vez que acierte siga mostrando rondas
     fun corrutina(){
         GlobalScope.launch(Dispatchers.Main) {
             //llamo al metodo ejecutar Secuencia para que me ejecute el juego
